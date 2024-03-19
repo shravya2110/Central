3ddb31442904ddc41e8bc25fdae715976f74bc03
Hriddha-Bhowmik <163643049+Hriddha-Bhowmik@users.noreply.github.com>
2024-03-19 15:13:48 +0530

    Update ChromeTest.java

--- src/test/java/firstproject/ChromeTest.java
+++ src/test/java/firstproject/ChromeTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+//changes by Hriddha
 public class ChromeTest {
 
 	public static void main(String[] args) throws InterruptedException {
